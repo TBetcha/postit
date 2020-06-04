9e0c59967502cf1e37a81b53520dd4eb2d40c3b6
Tbetcha <troy.boettger@gmail.com>
2020-06-03 20:18:54 -0400

    initial commit

---
+++ src/http.js
@@ -0,0 +1,47 @@
+/** @format */
+
+class SimpleHTTP {
+	//make get request
+	async get(url) {
+		const response = await fetch(url);
+
+		const resData = await response.json();
+		return resData;
+	}
+
+	//make post request
+
+	async post(url, data) {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'appliction/json',
+			},
+			body: JSON.stringify(data),
+		});
+	}
+
+	async put(url, data) {
+		const response = fetch(url, {
+			method: 'PUT',
+			headers: {
+				'Content-type': 'appliction/json',
+			},
+			body: JSON.stringify(data),
+		});
+	}
+
+	//HTTP delete request
+	async delete(url) {
+		const response = await fetch(url, {
+			method: 'DELETE',
+			headers: {
+				'Content-type': 'appliction/json',
+			},
+		});
+		const resData = await 'Resouce deleted';
+		return resData;
+	}
+}
+
+export const http = new SimpleHTTP();
